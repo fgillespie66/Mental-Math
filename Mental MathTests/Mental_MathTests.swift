968342d537ad7c8c5d5f9d036b6545cfa1513705
Fiona Gillespie <cflee4@fionas-mbp.fios-router.home>
2016-08-24 23:27:26 -0400

    Initial Commit

---
+++ Mental MathTests/Mental_MathTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mental_MathTests.swift
+//  Mental MathTests
+//
+//  Created by Fiona Gillespie on 8/24/16.
+//  Copyright © 2016 Fiona Gillespie. All rights reserved.
+//
+
+import XCTest
+@testable import Mental_Math
+
+class Mental_MathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
