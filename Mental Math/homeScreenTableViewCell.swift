96668b594daf93257b06bd5fc3e57aebf320fd23
Fiona Gillespie <cflee4@fionas-mbp.fios-router.home>
2016-09-11 23:09:32 -0400

    I added some new views to try to get the home screen working, but I cannot see the cell that would let me go to the arithmetic screen when the app is started.

---
+++ Mental Math/homeScreenTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  homeScreenTableViewCell.swift
+//  Mental Math
+//
+//  Created by Fiona Gillespie on 9/5/16.
+//  Copyright © 2016 Fiona Gillespie. All rights reserved.
+//
+
+import UIKit
+
+class homeScreenTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var arithmeticTableViewCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mental Math/homeScreenViewController.swift
@@ -0,0 +1,49 @@
+//
+//  homeScreenViewController.swift
+//  Mental Math
+//
+//  Created by Fiona Gillespie on 9/5/16.
+//  Copyright © 2016 Fiona Gillespie. All rights reserved.
+//
+
+import UIKit
+
+class homeScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var table: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // your cell coding
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // cell selected code here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
